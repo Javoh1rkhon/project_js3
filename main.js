aaf8006ec51e8c88177a34e2d74cf6e662c38504
javoh1rkhon <rustamovjavohirkhon@gmail.com>
2023-10-09 14:02:39 +0500

    js_project

---
+++ main.js
@@ -0,0 +1,30 @@
+// object = maulumbir elementning to'liq malumpotlari
+// Object = {key / nomi : "qiymat" }
+
+let shkof = {
+    polka: 4,
+    eshig: 2,
+    ruchka: 4,
+    tortma: 2,
+    yangi: true,
+    price: "120 $",
+    delivery: false,
+}
+
+// console.log(shkof);
+// console.log(shkof.price);
+
+// shkof.price = "150 $"
+
+// console.log(shkof);
+// console.log(shkof.price);
+
+// shkof.owner = 'Chikko'
+
+let a = [[[], [[{ t: "Javohirkhon" }]], [], [[[{ n: { b: [{ s: "salom" }] } }]], [[{ d: "mening" }]],],], { h: 'ismim' }]
+
+console.log(a[0][3][0][0][0].n.b[0].s, a[0][3][1][0][0].d, a[1].h, a[0][1][0][0].t);
+// console.log(a[0][3][1][0][0].d); 
+// console.log(a[1].h);
+// console.log(a[0][1][0][0].t);
+console.log(a);
